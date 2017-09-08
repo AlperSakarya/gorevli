7f62c976d6b51ba8306d82a37495c2940d802c48
root <root@cdr.callocean.com>
2017-09-08 05:07:31 -0400

    xyz

--- routes.py
+++ routes.py
@@ -114,7 +114,7 @@ def send_sms_message():
             if type(i.phone_number) == str:
                 member_number = re.sub("[^0-9]", "", i.phone_number)
                 message = client.api.account.messages.create(to=member_number, from_=from_number, body=sms_message)
-                time.sleep(1)
+                time.sleep(3)
                 return render_template('gorevli-paneli.html', api_response=api_response,
                                        success_message="SMS was sent to all members!")
     except ApiException as e:
